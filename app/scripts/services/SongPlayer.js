48385a9e6122e1d078b04c8288306fd39aa9860e
danilomartins <dglmartins@outlook.com>
2016-10-12 13:11:07 +0800

    assignment 7 services completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -30,19 +30,46 @@
 			currentSong = song;
 		};
 
+/**
+* @function playSong
+* @desc plays the currentBuzzObject and sets the playing property of the song to true  
+* @param {Object} song
+*/
 		
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+			
+		};
+		
+		
+/**
+* @function pauseSong
+* @desc pauses the currentBuzzObject and sets the playing property of the song to false  
+* @param {Object} song
+*/
+		var pauseSong = function(song) {
+			currentBuzzObject.pause();
+			song.playing = false;
+			
+		};
+		
+/**
+* SongPlayer.play property - @function
+* @desc if current song is not song passed, sets and plays the new song. If current song is the song passed, plays song if song is paused. 
+* @param {Object} song
+*/		
 		
 		SongPlayer.play = function(song) {
 			
 			if(currentSong !== song) {
 				setSong(song);
 			
-				currentBuzzObject.play();
-				song.playing = true;
+				playSong(song);
 			
 			} else if (currentSong === song) {
 				if (currentBuzzObject.isPaused()) {
-					currentBuzzObject.play();
+					playSong(song);
 					
 				}
 				
@@ -50,10 +77,14 @@
 			}
 			
 		};
-		
+
+/**
+* SongPlayer.pause property - @function
+* @desc pauses song passed. 
+* @param {Object} song
+*/
 		SongPlayer.pause = function(song) {
-			currentBuzzObject.pause();
-			song.playing = false;
+			pauseSong(song);
 			
 			
 		};
